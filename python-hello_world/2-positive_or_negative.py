ee9babe108d313ac003f959c617399e10690256e
MARVEL003 <chimchetarammarvel@gmail.com>
2023-08-24 10:11:03 -0700

    commit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,8 +4,9 @@ import random
 number = random.randint(-10, 10)
 
 if number > 0:
-    print("Correct output - positive")
+    print("{} is positive".format(number))
 elif number == 0:
-    print("Correct output - zero")
+    print("{} is zero".format(number))
 else:
-    print("Correct output - negative")
+    print("{} is negative".format(number))
+
