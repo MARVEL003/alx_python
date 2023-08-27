9849eb63956e79c23fdfba84bc61199a7e3d71b8
MARVEL003 <chimchetarammarvel@gmail.com>
2023-08-27 15:58:03 -0700

    commit

--- python-functions/3-main.py
+++ python-functions/3-main.py
@@ -1,12 +1,7 @@
-def reverse_string(string):
-    reversed_string = ""
-    for char in string:
-        reversed_string = char + reversed_string
-    return reversed_string
+#!/usr/bin/env python3
+reverse_string = __import__('3-string').reverse_string
 
-# Test cases
 print(reverse_string("Hello"))
 print(reverse_string(""))
 print(reverse_string("madam"))
 print(reverse_string("Hello, World!"))
-
--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -4,9 +4,3 @@ def reverse_string(string):
         reversed_string = char + reversed_string
     return reversed_string
 
-# Test cases
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
-
