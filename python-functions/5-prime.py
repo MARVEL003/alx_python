cbabecf3b5af087125bf5ca8d0bc9a312dc31852
MARVEL003 <chimchetarammarvel@gmail.com>
2023-08-27 16:43:10 -0700

    commit

---
+++ python-functions/5-prime.py
@@ -0,0 +1,15 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
