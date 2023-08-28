375d7bafca1596627e529ecc51761bae20c94f29
MARVEL003 <chimchetarammarvel@gmail.com>
2023-08-27 17:23:22 -0700

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,13 @@
+# Define variables a and b
 a = 1
 b = 2
 
+# Import the add function from add_0.py
 from add_0 import add
 
+# Call the add function and store the result
 result = add(a, b)
-print(f"{a} + {b} = {result}")
+
+# Print the formatted result
+print("{} + {} = {}".format(a, b, result))
 
