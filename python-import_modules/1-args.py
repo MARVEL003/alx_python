d2056254d3685f112c00afd57a15e7a126cb1e48
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-18 11:56:39 +0100

    commit

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+import sys
+
+if_name_=="__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+        else:
+            print(f"{num_args}arguments{'s' if num_args > 1 else"}:")
+            for i,arg in enumerate(args, 1):
+                print(f"{i}:{arg}")
