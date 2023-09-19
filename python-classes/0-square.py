0d8b7e4fb63e32f9d934de1a14db930970908dae
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-19 10:22:47 +0100

    Commit Work

---
+++ python-classes/0-square.py
@@ -0,0 +1,22 @@
+class Square:
+    """This a class considered as a parent to all instances being made in this module """
+
+    def __init__(self, size):
+        """using a double underscore with a name after a function makes it private."""
+        self.__size = size
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+    """A function to a method showing the attribute my_square while raising an exception"""
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)
