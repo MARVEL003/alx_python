50f3f740d7784a180345bf719756a4041ff626cc
MARVEL <chimchetarammarvel@gmail.com>
2023-10-12 23:33:57 -0700

    C is fun!

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,34 @@
+"""
+importing from flask
+"""
+from flask import Flask
+from markupsafe import escape
+
+"""
+this is needed so flask knows where to look up resources
+"""
+app = Flask(__name__)
+
+"""
+this is a decorator used to show flask what URL it'll trigger
+"""
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>/", strict_slashes=False)
+def c(text):
+    # replace all underscores with spaces
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
