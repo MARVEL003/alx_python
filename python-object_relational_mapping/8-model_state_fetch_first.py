04d4f6d01fec855624c225e10fda791370d6c130
MARVEL <chimchetarammarvel@gmail.com>
2023-10-12 22:55:00 -0700

    8. First state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # fetch first state if it exists else print nothing
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
